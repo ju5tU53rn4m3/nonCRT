0c3f0bb595e39007fe837d0232a31857e9e71597
ju5tU53rn4m3 <134021557+ju5tU53rn4m3@users.noreply.github.com>
2023-05-20 01:01:09 +0300

    Update Filesystem.h

--- NCLib/NCLib/Core/Filesystem.h
+++ NCLib/NCLib/Core/Filesystem.h
@@ -4,25 +4,25 @@ namespace Filesystem {
 
     class FileReader {
     public:
-        FileReader(const char* filename) : file_handle_(CreateFileA(filename, GENERIC_READ, 0, NULL, OPEN_EXISTING, FILE_ATTRIBUTE_NORMAL, NULL)) {}
+        FileReader(const char* filename) : fileHandle_(CreateFileA(filename, GENERIC_READ, 0, NULL, OPEN_EXISTING, FILE_ATTRIBUTE_NORMAL, NULL)) {}
 
         ~FileReader() {
-            if (file_handle_ != INVALID_HANDLE_VALUE) {
-                CloseHandle(file_handle_);
+            if (fileHandle_ != INVALID_HANDLE_VALUE) {
+                CloseHandle(fileHandle_);
             }
         }
 
-        bool ReadLine(char* buffer, size_t buffer_size) {
-            if (file_handle_ == INVALID_HANDLE_VALUE) {
+        bool ReadLine(char* buffer, size_t bufferSize) {
+            if (fileHandle_ == INVALID_HANDLE_VALUE) {
                 return false;
             }
 
-            DWORD bytes_read;
+            DWORD bytesRead;
             char ch = '\0';
             size_t i = 0;
 
-            while (i < buffer_size - 1 && ch != '\n') {
-                if (ReadFile(file_handle_, &ch, 1, &bytes_read, NULL) == 0 || bytes_read == 0) {
+            while (i < bufferSize - 1 && ch != '\n') {
+                if (ReadFile(fileHandle_, &ch, 1, &bytesRead, NULL) == 0 || bytesRead == 0) {
                     break;
                 }
 
@@ -35,6 +35,6 @@ namespace Filesystem {
         }
 
     private:
-        HANDLE file_handle_;
+        HANDLE fileHandle_;
     };
 }
