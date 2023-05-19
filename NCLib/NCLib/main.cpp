5a24efe8613b70d7969437bfb65d3144a513fc8b
ju5tU53rn4m3 <134021557+ju5tU53rn4m3@users.noreply.github.com>
2023-05-19 03:31:39 +0300

    Update main.cpp

--- NCLib/NCLib/main.cpp
+++ NCLib/NCLib/main.cpp
@@ -15,34 +15,27 @@ static const size_t npos = NonCRT::String::IString::npos;
 
 
 int _CustomEntry() {
-    //int в строку
     int myInt(5);
     iPrint(intToString(myInt));
 
-    //Циклы по массиву
 	IString it[] = { "str1", "str2", "str3" };
 	for (int i = 0; i < 3; i++) {
 		iPrint(it[i]);
 	}
 
-    // Проверка на пустую строку
     IString s("Hello, world!");
     IString emptySubstr = s.iSubstr(s.leng + 1, 5);
     iPrint(emptySubstr);
 
-    // Проверка на len = npos
     IString nposSubstr = s.iSubstr(7, IString::npos);
     iPrint(nposSubstr);
 
-    // Проверка на len < оставшейся длины строки
     IString lenSubstr = s.iSubstr(7, 3);
     iPrint( lenSubstr );
 
-    // Проверка на len > оставшейся длины строки
     IString remainingSubstr = s.iSubstr(7, 20);
     iPrint(remainingSubstr );
 
-    // Случайная строка и значения pos и len
     IString randomStr("abcdefg");
     size_t pos = 2;
     size_t len = 3;
@@ -50,12 +43,6 @@ int _CustomEntry() {
     iPrint( randomSubstr);
     iPrint(s + randomStr);
 
-    // Первое вхождение символа в строке
-    /*IString ptrStr("Hello, World!");    
-    iPrint(ptrStr.iSubstr(',', 2));*/
-
-
-    //Аналог регуляркам
     const char* str = "{{\"textTetxtTEXT:\" \"text1text2text3\"}; {\"wordWordWORD\": \"word1word2word3\"}; {\"textTetxtTEXT:\" \"text4text5text6\"}; {\"myString:\" \"MyString!!!\"}; {\"textTetxtTEXT:\" \"text7text8text9\"};}";
     const char* searchStr = "\"myString:\" ";
     size_t startIndex = IString(str).iFind(searchStr);
